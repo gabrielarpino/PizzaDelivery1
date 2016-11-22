8c5c2cdfa08f818c9d6d402fc4db2f26d1fa8268
gabrielarpino <arpino.gabriel@gmail.com>
2016-11-22 17:57:09 -0500

    updated follow road

--- src/followRoadToHouse.java
+++ src/followRoadToHouse.java
@@ -9,7 +9,7 @@ import lejos.hardware.lcd.*;
 
 public class followRoadToHouse {
 
-	// Has to follow road to house, using PID control. Has to be smooth because
+	// Has to follow road to house, using P control. Has to be smooth because
 	// the ultrasonic sensor has to be able to count houses.
 
 	// Reference to Sensors/Motors
@@ -27,70 +27,65 @@ public class followRoadToHouse {
 		// Define input sensors
 		static EV3UltrasonicSensor ultrasonic = new EV3UltrasonicSensor(SensorPort.S1);
 		static EV3ColorSensor color = new EV3ColorSensor(SensorPort.S2);
+		ultrasonic.
+
+		int sampleSize = color.sampleSize();
+		float[] redsample = new float[sampleSize];
+		color.getRedMode().fetchSample(redsample, 0); //not sure if RedMode matters
 
 		LCD.clear();
 		double correction;
-		double integral = 0;
-		double derivative;
-		double lasterror = 0;
-		double desired = 0.25;
-		double Kp = 20000;
-		double Ki = 250;
-		double Kd = 100;
-		int speed = 50;
-		int angle = 10;
 
-		int atHouse = 0;
 		int currentHouse = 0;
+		int atDesiredHouse = 0;
+		int threshold = 50;
 
-
-		while (!atHouse) 
+		while (!atDesiredHouse) 
 		{
-			// Follow PID control algorithm
+			// Run P control
 
 			int sampleSize = color.sampleSize();
 			float[] redsample = new float[sampleSize];
 			color.getRedMode().fetchSample(redsample, 0); //not sure if RedMode matters
 			//added code
+			double desired = 0.25;
+			double Kp = 20000;
+			
 			double actual = redsample[0]; // not sure if sampleSize is the colour sensor value
 			double error = desired - actual;
-			integral = integral + error;
-			derivative = error - lasterror;
-			if (Button.UP.isDown())
-			{
-				integral = 0;
-			}
-			if (Button.DOWN.isDown())
-			{
-				Kp = 0;
-			}
-			if (Button.RIGHT.isDown())
-			{
-				Kp = Kp + 50;
-			}
-			if (Button.LEFT.isDown())
-			{
-				Kp = Kp - 50;
-			}
-			correction = Kp*error + Ki*integral+Kd*derivative; //negative
+			correction = error*Kp; //negative
 			turn(correction);
 			//end added code
 			LCD.clear();
 			//System.out.println(redsample[0]);
-			System.out.println(correction, speed, angle);
-			System.out.println("Kp");
-			System.out.println(Kp);
-			lasterror = error;
-			if (error*lasterror < 0)
-			{
-				integral = 0;
-			}
+			System.out.println(correction);
 
-			currentHouse = checkAtHouse();
+			currentHouse = houseUpdate(ultrasonic, currentHouse, threshold)
 
 
 		}
 		color.close();
+	}
+
+	public static void turn(double direction) throws Exception {		
+		if (direction > 0) {
+			int speed = (int) direction;
+			Motor.B.setSpeed(0);
+			Motor.C.setSpeed(speed); // one wheel turns faster
+		}
+		else if (direction < 0) {
+			int speed = (int) direction*(-1);			
+			Motor.B.setSpeed(speed);
+			Motor.C.setSpeed(0);
+		}
+		else {
+			Motor.B.setSpeed(70);
+			Motor.C.setSpeed(70);
+		}
+		Motor.B.rotate(5,true);
+		Motor.C.rotate(5,true);
+			Motor.B.setSpeed(10);
+			Motor.C.setSpeed(10);
 	}
 
 	public static void turn(double direction, int speed, int angle) throws Exception 
@@ -101,9 +96,11 @@ public class followRoadToHouse {
 		Motor.C.rotate(angle,true);
 	}
 
-	public static void checkAtHouse(EV3UltrasonicSensor ultrasonic, int currentHouse){
+	public static void houseUpdate(EV3UltrasonicSensor ultrasonic, int currentHouse, int threshold){
 
-		
-		
+
+
+
+
 	}
 }
