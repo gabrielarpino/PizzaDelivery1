b55532d273ef3e487f8217c33bfcda84f4f52887
Sean Doughty <seandoughty@Seans-MacBook-Air.local>
2016-11-24 16:13:48 -0500

    add drop off pizza

---
+++ src/dropOffPizza.java
@@ -0,0 +1,16 @@
+import lejos.hardware.motor.Motor;
+
+public class dropOffPizza {
+	
+	public static double[] main(void);
+	{
+		int armdown = -150;
+		int armspeed = 150;
+		
+		
+		//dropping pizza
+		armMotor.speed = armspeed;
+		armMotor.rotate = armdown;
+}
+
+}
